24f29a1d84f924a628d834fe5291ac7c088727a7
artemenko_pa <artemenko_pa@magnit.ru>
2022-11-09 14:48:23 +0300

    Добавлен модуль SBA

---
+++ magreport-admin/src/test/java/ru/magnit/magreportadmin/MagreportAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.magnit.magreportadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagreportAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
