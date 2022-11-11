bbfea08eb8ba0fd7f542baaf9c1b1ba046038d5c
Vladimir Sukhov <suhov_vb@magnit.ru>
2022-11-11 13:51:37 +0300

    corrected

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export REACT_APP_VERSION=`cat version`
 mvn package
 cp -f .\magreport-backend\target\magreport-backend-2.0.jar $MAGREPORT_HOME\magreport.jar
 cp -f .\magreport-backend\run.sh $MAGREPORT_HOME\run.sh
-cp -f .\magreport-backend\run-magreport-admin.sh $MAGREPORT_HOME\run-magreport-admin.sh
 cp -f .\version $MAGREPORT_HOME\version
 cp -f .\docs\user-manual\src\user-manual.pdf $MAGREPORT_HOME\user-manual.pdf
-cp -f .\magreport-admin\target\magreport-admin-0.0.1-SNAPSHOT.jar $MAGREPORT_HOME\magreport-admin.jar
+cp -f .\magreport-admin\target\magreport-admin-0.0.1-SNAPSHOT.jar $MAGREPORT_HOME\magreport-admin\magreport-admin.jar
+cp -f .\magreport-admin\run-magreport-admin.sh $MAGREPORT_HOME\magreport-admin\run-magreport-admin.sh
