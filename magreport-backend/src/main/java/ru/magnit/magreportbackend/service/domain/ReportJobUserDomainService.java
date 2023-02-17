dd36cfbc0a7f701ee8d4b3139f6cf6237fb6ab6f
vyushin_aa <vyushin_aa@magnit.ru>
2023-02-17 16:36:46 +0300

    Обновил ответы сервисов /report-job/get-all-my-jobs                 /report-job/get-all-jobs

--- magreport-backend/src/main/java/ru/magnit/magreportbackend/service/domain/ReportJobUserDomainService.java
+++ magreport-backend/src/main/java/ru/magnit/magreportbackend/service/domain/ReportJobUserDomainService.java
@@ -8,7 +8,9 @@ import ru.magnit.magreportbackend.domain.reportjob.ReportJobUserType;
 import ru.magnit.magreportbackend.domain.reportjob.ReportJobUserTypeEnum;
 import ru.magnit.magreportbackend.domain.user.User;
 import ru.magnit.magreportbackend.dto.response.user.UserResponse;
+import ru.magnit.magreportbackend.dto.response.user.UserShortResponse;
 import ru.magnit.magreportbackend.mapper.auth.UserResponseMapper;
+import ru.magnit.magreportbackend.mapper.auth.UserShortResponseMapper;
 import ru.magnit.magreportbackend.repository.ReportJobUserRepository;
 
 import javax.transaction.Transactional;
@@ -20,6 +22,8 @@ public class ReportJobUserDomainService {
 
     private final ReportJobUserRepository repository;
     private final UserResponseMapper userResponseMapper;
+    private final UserShortResponseMapper userShortResponseMapper;
+
 
     @Transactional
     public void addUsersJob( ReportJobUserTypeEnum type, Long jobId, List<UserResponse> users) {
@@ -39,6 +43,14 @@ public class ReportJobUserDomainService {
     @Transactional
     public List<UserResponse> getUsersJob(Long jobId){
        return userResponseMapper.from(repository.findAllForJob(jobId)
+                .stream()
+                .map(ReportJobUser::getUser)
+                .toList());
+    }
+
+    @Transactional
+    public List<UserShortResponse> getShortUsersJob(Long jobId){
+        return userShortResponseMapper.from(repository.findAllForJob(jobId)
                 .stream()
                 .map(ReportJobUser::getUser)
                 .toList());
