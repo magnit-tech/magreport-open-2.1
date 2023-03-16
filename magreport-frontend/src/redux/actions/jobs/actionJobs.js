a7f53a9e5c62c931bbeb51c1f2a61d711021d583
lisyanskiy_ma <lisyanskiy_ma@magnit.ru>
2023-03-16 18:29:54 +0300

    feat: add btn for shareList for job item

--- magreport-frontend/src/redux/actions/jobs/actionJobs.js
+++ magreport-frontend/src/redux/actions/jobs/actionJobs.js
@@ -5,7 +5,7 @@ import {FolderItemTypes} from 'main/FolderContent/FolderItemTypes';
 import { JOBS_FILTER, USERS_JOBS_FILTER, JOB_CANCEL, JOB_CANCELED, JOB_CANCEL_FAILED, JOB_SQL_CLICK, 
         JOB_DIALOG_CLOSE , JOB_SQL_LOADED, JOB_SQL_LOAD_FAILED, 
         JOB_STATUS_HISTORY_CLICK, JOB_STATUS_HISTORY_LOADED, JOB_STATUS_HISTORY_LOAD_FAILED, 
-        JOB_ADD_COMMENT, JOB_ADD_COMMENT_SUCCESS, JOB_ADD_COMMENT_FAILED} from '../../reduxTypes'
+        JOB_ADD_COMMENT, JOB_ADD_COMMENT_SUCCESS, JOB_ADD_COMMENT_FAILED, JOB_SHARE_LIST_CLICK} from '../../reduxTypes'
 
 export function actionFilterJobs(itemsType, jobsFilters){
     return {
@@ -91,6 +91,7 @@ function handleClickStatusHistory(magrepResponse){
 }
 
 export const actionShowStatusHistory = (itemsType, titleName, id) => {
+
     if (itemsType === FolderItemTypes.job || itemsType === FolderItemTypes.userJobs) {
         dataHub.reportJobController.getHistory(id, handleClickStatusHistory)
     }
@@ -133,4 +134,14 @@ export function actionJobAddComment(itemsType, jobId, jobIndex, comment){
         jobId,
         comment
     })
+}
+
+export const actionShowShareList = (itemsType, titleName, id, data) => {
+    return {
+        type: JOB_SHARE_LIST_CLICK,
+        itemsType,
+        titleName,
+        id,
+        data: data,
+    }
 }
