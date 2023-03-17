e40a21f2a178b17f94694aad7e88d34fd51c099d
vyushin_aa <vyushin_aa@magnit.ru>
2023-03-17 11:33:53 +0300

    Исправил логику выдачи прав к папкам источников данных, наборов данных, шаблонов и фильтров

--- magreport-backend/src/main/java/ru/magnit/magreportbackend/service/FolderPermissionsService.java
+++ magreport-backend/src/main/java/ru/magnit/magreportbackend/service/FolderPermissionsService.java
@@ -17,8 +17,15 @@ import ru.magnit.magreportbackend.dto.response.permission.FolderPermissionsRespo
 import ru.magnit.magreportbackend.dto.response.permission.ReportFolderPermissionsResponse;
 import ru.magnit.magreportbackend.dto.response.permission.RolePermissionResponse;
 import ru.magnit.magreportbackend.dto.response.permission.SecurityFilterFolderPermissionsResponse;
+import ru.magnit.magreportbackend.service.domain.DataSetDomainService;
+import ru.magnit.magreportbackend.service.domain.DataSourceDomainService;
+import ru.magnit.magreportbackend.service.domain.ExcelTemplateDomainService;
+import ru.magnit.magreportbackend.service.domain.FilterInstanceDomainService;
+import ru.magnit.magreportbackend.service.domain.FilterTemplateDomainService;
 import ru.magnit.magreportbackend.service.domain.FolderDomainService;
 import ru.magnit.magreportbackend.service.domain.FolderPermissionsDomainService;
+import ru.magnit.magreportbackend.service.domain.ReportDomainService;
+import ru.magnit.magreportbackend.service.domain.SecurityFilterDomainService;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -32,30 +39,32 @@ public class FolderPermissionsService {
     private final FolderPermissionsDomainService service;
 
     private final FolderDomainService folderDomainService;
+    private final ReportDomainService reportDomainService;
+    private final DataSourceDomainService dataSourceDomainService;
+    private final DataSetDomainService dataSetDomainService;
+    private final ExcelTemplateDomainService excelTemplateDomainService;
+    private final FilterInstanceDomainService filterInstanceDomainService;
+    private final FilterTemplateDomainService filterTemplateDomainService;
+    private final SecurityFilterDomainService securityFilterDomainService;
+
+
 
     public FolderPermissionsResponse getFolderReportPermissions(FolderRequest request) {
-
         return service.getFolderReportPermissions(request.getId());
     }
 
     public FolderPermissionsResponse setFolderReportPermissions(FolderPermissionSetRequest request) {
 
         var currentFolderRolePermission = service.getFolderReportPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
         Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        request.setDownSetPermissions(true);
-        request.setUpSetPermissions(true);
-
-        if (request.isDownSetPermissions()) {
+        if (request.isDownSetPermissions())
             folders.addAll(service.getFolderReportBranch(request.getFolderId()));
-        }
-
-        if (request.isUpSetPermissions()) {
+        if (request.isUpSetPermissions())
             folders.addAll(folderDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
-        }
-
-        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
 
         for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
             if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
@@ -76,46 +85,105 @@ public class FolderPermissionsService {
     }
 
     public ReportFolderPermissionsResponse getReportFolderPermissions(FolderRequest request) {
-
         return service.getReportFolderPermissions(request.getId());
     }
 
     public ReportFolderPermissionsResponse setReportFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getReportFolderBranch(request.getFolderId());
+
+        var currentFolderRolePermission = service.getReportFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearReportFoldersPermissions(folders);
-        service.setReportFolderPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getReportFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(reportDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateReportFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteReportFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteReportFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addReportFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
 
         return service.getReportFolderPermissions(request.getFolderId());
     }
 
     public DataSourceFolderPermissionsResponse getDataSourceFolderPermissions(FolderRequest request) {
-
         return service.getDataSourceFolderPermissions(request.getId());
     }
 
     public DataSourceFolderPermissionsResponse setDataSourceFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getDataSourceFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getDataSourceFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearDataSourceFoldersPermissions(folders);
-        service.setDataSourceFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getDataSourceFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(dataSourceDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateDataSourceFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteDataSourceFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteDataSourceFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addDataSourceFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
 
         return service.getDataSourceFolderPermissions(request.getFolderId());
     }
 
     public DataSetFolderPermissionsResponse getDataSetFolderPermissions(FolderRequest request) {
-
         return service.getDataSetFolderPermissions(request.getId());
     }
 
     public DataSetFolderPermissionsResponse setDataSetFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getDataSetFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getDataSetFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearDataSetFoldersPermissions(folders);
-        service.setDataSetFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getDataSetFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(dataSetDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateDataSetFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteDataSetFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteDataSetFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addDataSetFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
+
 
         return service.getDataSetFolderPermissions(request.getFolderId());
     }
@@ -126,26 +194,65 @@ public class FolderPermissionsService {
     }
 
     public ExcelTemplateFolderPermissionsResponse setExcelTemplateFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getExcelTemplateFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getExcelTemplateFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearExcelTemplateFoldersPermissions(folders);
-        service.setExcelTemplateFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getExcelTemplateFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(excelTemplateDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateExcelTemplateFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteExcelTemplateFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteExcelTemplateFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addExcelTemplateFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
 
         return service.getExcelTemplateFolderPermissions(request.getFolderId());
     }
 
     public FilterInstanceFolderPermissionsResponse getFilterInstanceFolderPermissions(FolderRequest request) {
-
         return service.getFilterInstanceFolderPermissions(request.getId());
     }
 
     public FilterInstanceFolderPermissionsResponse setFilterInstanceFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getFilterInstanceFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getFilterInstanceFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearFilterInstanceFoldersPermissions(folders);
-        service.setFilterInstanceFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getFilterInstanceFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(filterInstanceDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateFilterInstanceFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteFilterInstanceFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteFilterInstanceFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addFilterInstanceFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
 
         return service.getFilterInstanceFolderPermissions(request.getFolderId());
     }
@@ -156,11 +263,31 @@ public class FolderPermissionsService {
     }
 
     public FilterTemplateFolderPermissionsResponse setFilterTemplateFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getFilterTemplateFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getFilterTemplateFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearFilterTemplateFoldersPermissions(folders);
-        service.setFilterTemplateFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getFilterTemplateFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(filterTemplateDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
+
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateFilterTemplateFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteFilterTemplateFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteFilterTemplateFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addFilterTemplateFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
 
         return service.getFilterTemplateFolderPermissions(request.getFolderId());
     }
@@ -171,18 +298,35 @@ public class FolderPermissionsService {
     }
 
     public SecurityFilterFolderPermissionsResponse setSecurityFilterFolderPermissions(FolderPermissionSetRequest request) {
-        final var folders = service.getSecurityFilterFolderBranch(request.getFolderId());
+        var currentFolderRolePermission = service.getSecurityFilterFolderPermissions(request.getFolderId());
+        var mappingFolderRolePermissions = request.getRoles().stream().collect(Collectors.toMap(RoleAddPermissionRequest::getRoleId, RoleAddPermissionRequest::getPermissions));
+
+        Set<Long> folders = new HashSet<>();
         folders.add(request.getFolderId());
 
-        service.clearSecurityFilterFoldersPermissions(folders);
-        service.setSecurityFilterFoldersPermissions(folders, request);
+        if (request.isDownSetPermissions())
+            folders.addAll(service.getSecurityFilterFolderBranch(request.getFolderId()));
+        if (request.isUpSetPermissions())
+            folders.addAll(securityFilterDomainService.getPathToFolder(request.getFolderId()).stream().map(FolderNodeResponse::id).toList());
 
+        for (RolePermissionResponse r : currentFolderRolePermission.rolePermissions()) {
+            if (mappingFolderRolePermissions.containsKey(r.role().getId())) {
+                var newPermission = mappingFolderRolePermissions.get(r.role().getId());
+                if (!newPermission.equals(r.permissions()))
+                    service.updateSecurityFilterFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(r.role().getId(), newPermission));
+                mappingFolderRolePermissions.remove(r.role().getId());
+            } else
+                service.deleteSecurityFilterFolderPermittedToRole(new ArrayList<>(folders), r.role().getId());
+        }
+
+        mappingFolderRolePermissions.forEach((key, value) -> {
+            service.deleteSecurityFilterFolderPermittedToRole(new ArrayList<>(folders), key);
+            service.addSecurityFilterFolderPermissions(new ArrayList<>(folders), new RoleAddPermissionRequest(key, value));
+        });
         return service.getSecurityFilterFolderPermissions(request.getFolderId());
     }
 
     public FolderPermissionCheckResponse checkFolderPermission(PermissionCheckRequest request) {
         return service.checkFolderPermission(request);
     }
-
-
 }
