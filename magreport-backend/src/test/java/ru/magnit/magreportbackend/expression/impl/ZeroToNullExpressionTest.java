24e47319633ea05802ebc6ff2422f0345f715d9d
Павел Артеменко <artemenko_pa@magnit.ru>
2023-03-27 12:10:38 +0300

    Resolve "Реализовать функцию ZERO_TO_NULL(X) для производных полей"

---
+++ magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/ZeroToNullExpressionTest.java
@@ -0,0 +1,49 @@
+package ru.magnit.magreportbackend.expression.impl;
+
+import org.junit.jupiter.api.Test;
+import ru.magnit.magreportbackend.domain.dataset.DataTypeEnum;
+import ru.magnit.magreportbackend.domain.enums.Expressions;
+import ru.magnit.magreportbackend.dto.response.derivedfield.FieldExpressionResponse;
+import ru.magnit.magreportbackend.expression.ExpressionCreationContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZeroToNullExpressionTest {
+    @Test
+    void ZeroToNullIntegerTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+            .setType(Expressions.ZERO_TO_NULL_EXPRESSION)
+            .setParameters(List.of(
+                new FieldExpressionResponse()
+                    .setType(Expressions.CONSTANT_VALUE)
+                    .setConstantType(DataTypeEnum.INTEGER)
+                    .setConstantValue("0")
+            ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertNull(expressionResult.getL());
+        assertEquals(DataTypeEnum.INTEGER, expressionResult.getR());
+    }
+
+    @Test
+    void ZeroToNullDoubleTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+            .setType(Expressions.ZERO_TO_NULL_EXPRESSION)
+            .setParameters(List.of(
+                new FieldExpressionResponse()
+                    .setType(Expressions.CONSTANT_VALUE)
+                    .setConstantType(DataTypeEnum.DOUBLE)
+                    .setConstantValue("0")
+            ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertNull(expressionResult.getL());
+        assertEquals(DataTypeEnum.DOUBLE, expressionResult.getR());
+    }
+}
