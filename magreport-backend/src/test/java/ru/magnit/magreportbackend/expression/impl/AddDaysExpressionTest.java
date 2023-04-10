7e318a32e3d76ebaeeb7c4d44f48513afc40ffe6
artemenko_pa <artemenko_pa@magnit.ru>
2023-04-10 12:27:00 +0300

    Добавлен тест на вычитание дней

--- magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/AddDaysExpressionTest.java
+++ magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/AddDaysExpressionTest.java
@@ -32,4 +32,26 @@ class AddDaysExpressionTest {
         assertEquals("2023-03-29", expressionResult.getL());
         assertEquals(DataTypeEnum.DATE, expressionResult.getR());
     }
+
+    @Test
+    void SubtractDaysTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+                .setType(Expressions.ADD_DAYS)
+                .setParameters(List.of(
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.DATE)
+                                .setConstantValue("2023-03-28"),
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue("-1")
+                ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertEquals("2023-03-27", expressionResult.getL());
+        assertEquals(DataTypeEnum.DATE, expressionResult.getR());
+    }
 }
