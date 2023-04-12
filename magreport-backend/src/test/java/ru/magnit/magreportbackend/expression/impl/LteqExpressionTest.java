2c39a781f8eb1d50d6f8f27c4c3d0be137ba8500
Павел Артеменко <artemenko_pa@magnit.ru>
2023-04-12 14:04:28 +0300

    Resolve "Для функций сравнения реализовать возможность сравнения INTEGER и DOUBLE"

--- magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/LteqExpressionTest.java
+++ magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/LteqExpressionTest.java
@@ -32,4 +32,26 @@ class LteqExpressionTest {
         assertEquals("true", expressionResult.getL());
         assertEquals(DataTypeEnum.BOOLEAN, expressionResult.getR());
     }
+
+    @Test
+    void LteqIntDoubleTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+                .setType(Expressions.LTEQ)
+                .setParameters(List.of(
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue("2"),
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.DOUBLE)
+                                .setConstantValue("2.1")
+                ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertEquals("true", expressionResult.getL());
+        assertEquals(DataTypeEnum.BOOLEAN, expressionResult.getR());
+    }
 }
--- magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/NeqExpressionTest.java
+++ magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/NeqExpressionTest.java
@@ -32,4 +32,26 @@ class NeqExpressionTest {
         assertEquals("false", expressionResult.getL());
         assertEquals(DataTypeEnum.BOOLEAN, expressionResult.getR());
     }
+
+    @Test
+    void NeqIntDoubleTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+                .setType(Expressions.NEQ)
+                .setParameters(List.of(
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue("2"),
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.DOUBLE)
+                                .setConstantValue("2")
+                ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertEquals("false", expressionResult.getL());
+        assertEquals(DataTypeEnum.BOOLEAN, expressionResult.getR());
+    }
 }
