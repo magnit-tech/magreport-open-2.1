ecf8bf51b5272e69e6d6cc8ac547f6696d6f9a52
dyadchenko_lo <dyadchenko_lo@magnit.ru>
2023-05-10 12:22:42 +0300

    close #320

--- magreport-frontend/src/main/Main/Development/Designer/DesignerTextField.js
+++ magreport-frontend/src/main/Main/Development/Designer/DesignerTextField.js
@@ -7,6 +7,7 @@ import ClearIcon from '@material-ui/icons/Clear';
 import Tooltip from '@material-ui/core/Tooltip';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import {DesignerCSS} from './DesignerCSS';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 
 /**
  * @callback onChange
@@ -35,11 +36,6 @@ export default function DesignerTextField(props){
 
     //const [designerTextValue, setDesignerTextValue] = useState(props.value === 0 || props.value ? props.value : '');
 
-    function handleChange(value){
-       // setDesignerTextValue(value);
-        props.onChange(value);
-    }
-
     const classes = DesignerCSS(); 
 
     return (
@@ -58,27 +54,49 @@ export default function DesignerTextField(props){
             error = {!!props.error}
             disabled = {props.disabled}
             size={props.size}
-          //  InputProps = {props.inputProps}
             InputLabelProps = {props.InputLabelProps}
             helperText = {props.helperText}
             onInput  = {props.onInput ? (event) => props.onInput(event):()=>{} }
-            onChange = {event => {props.type === "file" ? handleChange(event) : handleChange(event.target.value)}}
+            onChange = {event => {props.type === "file" ? props.onChange(event) : props.onChange(event.target.value)}}
             InputProps={{ ...props.inputProps,
                 endAdornment: (
                     props.clearable ? 
-                    <InputAdornment position="end" /*className={classes.topInputAdornment}*/>      
-                        <Tooltip title="Очистить" placement="top">
-                            <IconButton 
-                                aria-label="clear"
-                                color='primary'
-                                size='small'
-                                onClick={()=> handleChange('')}
-                            >
-                                <ClearIcon fontSize='small' />
-                            </IconButton>
-                        </Tooltip>
-                    </InputAdornment>     
-                    : null   
+                        <InputAdornment>    
+                            <Tooltip title="Очистить" placement="top">
+                                <IconButton 
+                                    aria-label="clear"
+                                    color='primary'
+                                    size='small'
+                                    onClick={()=> props.onChange('')}
+                                >
+                                    <ClearIcon fontSize='small' />
+                                </IconButton>
+                            </Tooltip>
+                        </InputAdornment>
+                    : null
+                ),
+                startAdornment: ( 
+                    props.isFile ?
+                        <InputAdornment>
+                            <Tooltip title="Выбрать файл">
+                                {/* span не убирать, иначе сыпятся Warnings если disabled=true */}
+                                <span>
+                                    <IconButton 
+                                        aria-label="Select"
+                                        size='small'
+                                        component="label"
+                                    >
+                                        <AttachFileIcon/>
+                                        <input
+                                            type="file"
+                                            hidden
+                                            onChange={value => props.onChange(value)}
+                                        />
+                                        </IconButton>
+                                </span>
+                            </Tooltip>
+                        </InputAdornment>
+                    : null
                 ),
             }}
 
