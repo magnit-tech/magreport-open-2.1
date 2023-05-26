69e1e9c9e756f718116a572bf971fc78925a7129
vyushin_aa <vyushin_aa@magnit.ru>
2023-05-26 13:14:44 +0300

    Обновил логику token_input

--- magreport-backend/src/main/java/ru/magnit/magreportbackend/dto/inner/filter/FilterValueListRequestData.java
+++ magreport-backend/src/main/java/ru/magnit/magreportbackend/dto/inner/filter/FilterValueListRequestData.java
@@ -1,21 +1,18 @@
 package ru.magnit.magreportbackend.dto.inner.filter;
 
-import ru.magnit.magreportbackend.domain.dataset.DataTypeEnum;
 import ru.magnit.magreportbackend.dto.inner.datasource.DataSourceData;
 import ru.magnit.magreportbackend.dto.request.filterinstance.LikenessType;
+
+import java.util.List;
 
 public record FilterValueListRequestData(
         DataSourceData dataSource,
         String schemaName,
         String tableName,
-        String filterFieldName,
-        DataTypeEnum filterFieldType,
-        Long idFieldId,
-        String idFieldName,
-        Long codeFieldId,
-        String codeFieldName,
-        Long nameFieldId,
-        String nameFieldName,
+        List<FilterFieldRequestData> filterFields,
+        FilterFieldRequestData idField,
+        FilterFieldRequestData codeField,
+        List<FilterFieldRequestData> nameFields,
         boolean isCaseSensitive,
         LikenessType likenessType,
         long maxCount,
