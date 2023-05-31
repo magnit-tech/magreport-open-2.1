3c10e14705d065b76118f403150ee78641284dcc
vyushin_aa <vyushin_aa@magnit.ru>
2023-05-31 15:12:18 +0300

    Фикс сервиса get-fields-values

--- magreport-backend/src/main/java/ru/magnit/magreportbackend/service/domain/filter/CubeFilterGroup.java
+++ magreport-backend/src/main/java/ru/magnit/magreportbackend/service/domain/filter/CubeFilterGroup.java
@@ -4,22 +4,23 @@ import ru.magnit.magreportbackend.domain.enums.BinaryBooleanOperations;
 import ru.magnit.magreportbackend.dto.inner.olap.CubeData;
 import ru.magnit.magreportbackend.dto.request.olap.FilterGroup;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CubeFilterGroup implements CubeFilterNode{
 
-    private List<CubeFilterNode> childNodes = Collections.emptyList();
+    private final List<CubeFilterNode> childNodes = new ArrayList<>();
     private BinaryBooleanOperations operationType;
     private boolean invertResult;
 
     @Override
     public CubeFilterNode init(CubeData sourceCube, FilterGroup filterGroup) {
-        if (filterGroup.getChildGroups() != null && !filterGroup.getChildGroups().isEmpty()) {
-            childNodes = filterGroup.getChildGroups().stream().map(childGroup -> CubeFilters.createFilter(sourceCube, childGroup)).toList();
-        } else {
-            childNodes = filterGroup.getFilters().stream().map(filter -> CubeFilters.createFilter(sourceCube, filter)).toList();
-        }
+        if (filterGroup.getChildGroups() != null && !filterGroup.getChildGroups().isEmpty())
+            childNodes.addAll(filterGroup.getChildGroups().stream().map(childGroup -> CubeFilters.createFilter(sourceCube, childGroup)).toList());
+
+        if (!filterGroup.getFilters().isEmpty())
+            childNodes.addAll(filterGroup.getFilters().stream().map(filter -> CubeFilters.createFilter(sourceCube, filter)).toList());
+
         operationType = filterGroup.getOperationType();
         invertResult = filterGroup.isInvertResult();
         return this;
