95e3a4874a85aae89742dd2e763a8d4074960220
artemenko_pa <artemenko_pa@magnit.ru>
2023-07-20 13:56:30 +0300

    Пофикшен баг с null для вычитания

--- magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/SubtractExpressionTest.java
+++ magreport-backend/src/test/java/ru/magnit/magreportbackend/expression/impl/SubtractExpressionTest.java
@@ -67,4 +67,30 @@ class SubtractExpressionTest {
         assertEquals("13", expressionResult.getL());
         assertEquals(DataTypeEnum.INTEGER, expressionResult.getR());
     }
+
+    @Test
+    void subtractNullTest() {
+        final var sourceExpression = new FieldExpressionResponse()
+                .setType(Expressions.SUBTRACT)
+                .setParameters(List.of(
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue("15"),
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue(null),
+                        new FieldExpressionResponse()
+                                .setType(Expressions.CONSTANT_VALUE)
+                                .setConstantType(DataTypeEnum.INTEGER)
+                                .setConstantValue("-1")
+                ));
+
+        final var expression = sourceExpression.getType().init(sourceExpression, new ExpressionCreationContext(null, null, null, null));
+        final var expressionResult = expression.calculate(0);
+
+        assertNull(expressionResult.getL());
+        assertEquals(DataTypeEnum.INTEGER, expressionResult.getR());
+    }
 }
